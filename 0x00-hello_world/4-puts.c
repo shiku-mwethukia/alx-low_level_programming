b446bb1c24e6d9ce21897f1efa97d6b8c0ff8be9
shiku-mwethukia <marymwethukia65@gmail.com>
2022-04-07 16:53:03 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 /**
 *main - Entry point
-*  
-*Return: Always return 0(Success)
-*/ 
-int main(void) 
+*
+*Return: Always 0(Success)
+*/
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
