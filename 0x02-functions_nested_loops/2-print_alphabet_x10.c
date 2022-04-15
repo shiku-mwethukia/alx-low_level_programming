aec9677136319c0702609a1d80dbbedbc2676d55
shiku-mwethukia <marymwethukia65@gmail.com>
2022-04-15 07:26:57 -0700

    print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,27 @@
 #include "main.h"
+
 /**
-*print_alphabet_x10 - print alphabet
-*
-*Return: Always 0.
+ * print_alphabet_x10(void)
+ *
+ * Return: Always 0.
 */
 void print_alphabet_x10(void)
 {
 int alphabet;
 int count;
+
+
 count = 0;
 while (count < 10)
 {
-for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+for (alphabet = 'a'; alphabet = 'z'; alphabet++)
 {
 _putchar(alphabet);
 }
+
 count++;
-_putchar2-print_alphabet_x10.c('\n');
+|_putchar('\n');
 }
+
 }
+
