96d770392692035eed7b3ab50e500ae7b865c932
shiku-mwethukia <marymwethukia65@gmail.com>
2022-04-16 10:56:03 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,49 +1,47 @@
 #include "main.h"
 /**
- * times_table -prints tables
- *
- * Return: Always 0.
+*times_table - prints times table
+*Return:void
 */
 
 void times_table(void)
+{
 
-{
-int a;
+int a = 0;
 int b;
-int c;
-for (a = 0; a <= 9; a++)
+int rep;
+while (a <= 9)
 {
-for (b = 0; b <= 9; b++)
+
+b = 0;
+while (b <= 9)
 {
-c = a * b;
-if ((c / 10) == 0)
-{
+
+rep = a * b;
 if (b == 0)
 {
-_putchar ('0');
+_putchar('0' + rep);
 }
-if (b != 0)
+else if (rep < 10)
 {
-_putchar (' ');
-_putchar ((c % 10) + '0');
-}
-if (b < 9)
-{
-_putchar (',');
-_putchar (' ');
-}
+_putchar(' ');
+_putchar('0' + rep);
 }
 else
 {
-_putchar ((c / 10) + '0');
-_putchar ((c % 10) + '0');
-if (b < 9)
-{
-_putchar (',');
-_putchar (' ');
-}
-}
-}
-}
+_putchar('0' + rep / 10);
+_putchar('0' + rep % 10);
 }
 
+if (b < 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+b++;
+}
+_putchar('\n');
+a++;
+}
+
+}
