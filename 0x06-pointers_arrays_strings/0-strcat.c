8149d50cd2fcf3a8a925adf9a086020c62c04877
shiku-mwethukia <marymwethukia65@gmail.com>
2022-04-23 13:46:13 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
-**_strcat - function commute strings
-*@dest: param pointer to a char
-*src: param pointer to a char
-*Return: return value of dest
+ * *_strcat - function commute strings
+ * @dest: param pointer to a char
+ * src: param pointer to a char
+ * Return: return value of dest
 */
+
 char *_strcat(char *dest, char *src)
 {
 int a;
