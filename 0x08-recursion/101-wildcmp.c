208ee11ec059592c58061d89a41d42c2d5b0283e
shiku-mwethukia <marymwethukia65@gmail.com>
2022-04-29 02:14:46 +0300

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,7 +15,7 @@ return (1);
 if (*s1 == *s2)
 return (wildcmp(s1 + 1, s2 + 1));
 if (*s2 == '*')
-return (wildcmp(s1, s2 + 1) ||wildcmp(s1 + 1, s2));
+return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 return (0);
 }
 
