ca214873ec3fd9bbad27293791c8b064407f50d2
shiku-mwethukia <marymwethukia65@gmail.com>
2022-04-28 13:32:51 -0700

    mainh

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,12 +1,12 @@
-#ifndef_MAIN_H_
-#define_MAIN_H_
-int_putchar(char);
-void_puts_recursion(char *s);
-void_print_rev_recursion(char *s);
-int_strlen_recursion(char *s);
+#ifndef _MAIN_H_
+#define _MAIN_H_
+int _putchar(char);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
 int factorial(int n);
-int_pow_recursion(int x, int y);
-int_sqrt_recursion(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
