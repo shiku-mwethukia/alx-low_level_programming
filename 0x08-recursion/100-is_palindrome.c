c864022ce6a315e49bdafb0f442df21be7c3ac96
shiku-mwethukia <marymwethukia65@gmail.com>
2022-04-29 03:40:59 +0300

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,44 +1,50 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - returns length of a string
- * @s: string
- * Return: length of a string
-*/
+ * _strlen_recursion - size
+ * @s: pointer to string params
+ * Return: recursion
+ */
+
 int _strlen_recursion(char *s)
 {
-if (*s == '\0')
-return (0);
-else
-return (1 + _strlen_recursion(s + 1));
-}
-/**
- * comparator - compares each character of the string
- * @s: string
- * @n1: smallest iterator
- * @n2: biggest iterator
- * Return: .
-*/
-int comparator(char *s, int n1, int n2)
-{
-if (*(s + n1) == *(s + n2))
-{
-if (n1 == n2 || n1 == n2 + 1)
-return (1);
-return (0 + comparator(s, n1 + 1, n2 - 1));
-}
-return (0);
+	if (!*s)
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(++s));
 }
 
 /**
- *is_palindrome - detects if a string is a palindrome
- * @s: string
- *Return: 1 if s is a palindrome, 0 if not
-*/
-int is_palindrome - detects if a string is palindrome
+ * p1 - palindrome
+ * @s: pointer to string
+ * @l: position
+ * Return: boolena
+ */
+
+int p1(char *s, int l)
 {
-if (*s == '\0')
-return (1);
-return (comparator(s, 0, _strlen_recursion(s) - 1));
+	if (l < 1)
+	{
+		return (1);
+	}
+
+	if (*s == *(s + l))
+	{
+		return (p1(s + 1, l - 2));
+	}
+	return (0);
 }
- 
+
+/**
+ * is_palindrome - palindrome
+ * @s: pointer to string
+ * Return: recursion
+ */
+
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+
+	return (p1(s, len - 1));
+}
