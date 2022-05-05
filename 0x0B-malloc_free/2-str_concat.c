f1540ca8f8fccda1d39f7a5f6d0323dfcc9e4cda
shiku-mwethukia <marymwethukia65@gmail.com>
2022-05-06 00:20:25 +0300

    str concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -4,14 +4,18 @@
 *str_concat - links two strings
 *@s1: first string
 *@s2: second string
+*
 *Return: pointer of array of chars
 */
 char *strout;
 unsigned int a, b, c, limit;
+
 if (s1 == NULL)
 s1 = "";
+
 if (s2 == NULL)
 s2 = "";
+
 for (a = 0; s1[a] != '\0'; a++)
 ;
 for (b = 0; s2[b] != '\0'; b++)
