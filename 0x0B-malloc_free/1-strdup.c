57f983c0b1b127bdea5bcce8ff2709a6bdeab094
shiku-mwethukia <marymwethukia65@gmail.com>
2022-05-05 23:49:28 +0300

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,7 +16,7 @@ for (a = 0; str[a] != '\0'; a++)
 strout = (char *)malloc(sizeof(char) * (a + 1));
 if (strout == NULL)
 return (NULL);
-for (b = 0; b <= a b++)
+for (b = 0; b <= a; b++)
 strout[b] = str[b];
 return (strout);
 }
