25ef29993cbf3d29acbdb67fa97cbd6f062f3d40
shiku-mwethukia <marymwethukia65@gmail.com>
2022-05-13 01:08:50 +0300

    2.Main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-*main - prints file name
-*Return: Always 0
-*/
+ * main - prints the name of the file program was compiled from
+ * Return: Always 0
+ */
+
 int main(void)
 {
-printf("%s\n", _FILE_);
+printf("%s\n", __FILE__);
 return (0);
 }
