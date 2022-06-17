fd60bd50cb42e46246cc21fd79f052b0a29d974f
shiku-mwethukia <marymwethukia65@gmail.com>
2022-06-17 17:33:01 +0300

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,12 +6,12 @@
  */
 void print_binary(unsigned long int n)
 {
-int a, count = 0;
+int i, count = 0;
 unsigned long int current;
 
-for (a = 63; a >= 0; a--)
+for (i = 63; i >= 0; i--)
 {
-current = n >> a;
+current = n >> i;
 
 if (current & 1)
 {
