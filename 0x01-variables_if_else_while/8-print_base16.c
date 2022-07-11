6e8a3871c01e14fb80c71874f3ab7db58c9e08ea
shiku-mwethukia <marymwethukia65@gmail.com>
2022-07-11 15:04:57 +0300

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+int i;
+
+int ch;
+
+for (i = 48; i < 58; i++)
+
+putchar(i);
+
+for (ch = 'a'; ch <= 'f'; ch++)
+
+putchar(ch);
+
+putchar('\n');
+
+return (0);
+
+}
