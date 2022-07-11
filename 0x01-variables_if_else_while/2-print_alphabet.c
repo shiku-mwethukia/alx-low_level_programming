bb2f53416f6969af4667722d3103c04e82b26c4b
shiku-mwethukia <marymwethukia65@gmail.com>
2022-07-11 14:29:36 +0300

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for alphabets
+ *
+ * Return: Always 0 for Success
+ */
+
+
+int main(void)
+
+{
+
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+putchar(alphabet);
+}
+
+putchar('\n');
+
+return (0);
+
+}
